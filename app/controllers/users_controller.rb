751fbbf117a9475e4c56834ae89c08ddcf5d66a1
AndySingularity <andysingularity@gmail.com>
2014-03-01 20:53:24 +0400

    todo @user + пробую изменить размер дефолтного изображения

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,31 @@
 class UsersController < ApplicationController
 
+  before_filter do
+    if !user_signed_in? then
+      redirect_to new_user_session_path
+    end
+    @user = current_user
+  end
+
 	def index
 	end
-
 
 	def show
     if !user_signed_in? then
       redirect_to new_user_session_path
     end
-		@user = current_user
 	end
-
-
-  def create
-    @user = User.create(user_params)
-  end
-
-
 
   def user_params
     params.require(:user).permit(:avatar, :about)
   end
 
   def edit
-    @user = current_user
   end
 
   def update
-    # TODO: Переменную @user нужно сделать доступной для всех методов. DRY-метода ёпты!)
-    @user = current_user
     @user.update(user_params)
-    redirect_to edit_user_path
+    redirect_to user_path(@user.id)
   end
 
 end
